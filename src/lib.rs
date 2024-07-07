485a4cd69ac84e2ee0fa381a21cd320c36bf0d6c
Stephen Burwell <xstompx@gmail.com>
2024-07-07 11:17:38 -0400

    Added joint love_equinox

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use time::Date;
 
+
 /// Calculates your love Equinox and returns it as a time::Date
 /// You must supply two time::Dates for this function
 pub fn calculate_le(first_date: Date, birthday: Date) -> Result<Date,String> {
@@ -10,7 +11,7 @@ pub fn calculate_le(first_date: Date, birthday: Date) -> Result<Date,String> {
     let equinox = first_date + diff;
     Ok(equinox)
 }
-/// Calculates your love Equinox and prints to terminal
+/// Calculates your love Equinox and printlns to terminal
 /// You must supply two time::Dates for this function
 pub fn le_print(first_date: Date, birthday: Date) {
     if first_date < birthday{
@@ -22,3 +23,31 @@ pub fn le_print(first_date: Date, birthday: Date) {
     println!("{}", equinox);
 }
 
+/// Calculates you and your partner's join Love Equinox and
+/// returns it as a time::Date
+/// You must supply three time::Dates for this function
+pub fn calculate_joint_le(first_date: Date, birthday_one: Date, birthday_two: Date) -> Result<Date, String>{
+    if first_date < birthday_one || first_date < birthday_two{
+        return Err("Not everyone was born when the relationship started!".to_owned())
+    }
+    let diff_one = first_date - birthday_one;
+    let diff_two = first_date - birthday_two;
+    let diff = (diff_one + diff_two)/2;
+    let equinox = first_date + diff;
+    Ok(equinox)
+}
+/// Calculates you and your partner's join Love Equinox and
+/// printlns it to your terminal
+/// You must supply three time::Dates for this function
+pub fn print_joint_le(first_date: Date, birthday_one: Date, birthday_two: Date){
+    if first_date < birthday_one || first_date < birthday_two{
+        println!("Not everyone was born when the relationship started!");
+        return
+    }
+    let diff_one = first_date - birthday_one;
+    let diff_two = first_date - birthday_two;
+    let diff = (diff_one + diff_two)/2;
+    let equinox = first_date + diff;
+
+    println!("{:?}", equinox)
+}
